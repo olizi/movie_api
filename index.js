1ad7cd477f200fe7614e6fe8d9e986b3bd75ec86
olizi <o.zink@zink-projects.de>
2020-10-25 14:42:37 +0100

    adds endpoints and documentation

--- index.js
+++ index.js
@@ -1,61 +1,67 @@
 const express = require('express'),
+
+  // The body-parser middleware module allows you to read the “body” of HTTP requests within your request handlers simply by using the code req.body.
+  bodyParser = require('body-parser'),
+
+  // With Universally Unique Identifier module installed, you’re able to generate a unique ID at any point in time by simply using the code uuid.v4().
+  uuid = require('uuid');
+
+  // HTTP request logger middleware 
   morgan = require('morgan');
 
 const app = express();
 
-let myTopTenMovies = [
-  {
-    title: 'Pulp Fiction',
-    cast: 'John Travolta, Samuel L. Jackson, Bruce Willis, Uma Thurman'
-  },
-  {
-    title: 'Fargo',
-    cast: 'Frances McDormand, William H. Macy, Steve Buscemi, Harve Presnell'
-  },
-  {
-    title: 'Inception',
-    cast: 'Leonardo DiCaprio, Joseph Gordon-Levitt, Tom Hardy, Marion Cotillard'
-  },
-  {
-    title: 'Interstellar',
-    cast: 'Matthew McConaughey, Anne Hathaway, David Gyasi'
-  },
-  {
-    title: 'The Big Lebowski',
-    cast: 'Jeff Bridges, John Goodman, Steve Buscemi'
-  },
-  {
-    title: 'Parasite',
-    cast: 'Song Kang-ho, Lee Sun-kyun, Cho Yeo-jeong, Jang Hye-jin'
-  },
-  {
-    title: 'Bang Boom Bang',
-    cast: 'Oliver Korittke, Markus Knüfken, Ralf Richter, Sabine Kaack'
-  },
-  {
-    title: 'Arizona Dream',
-    cast: 'Johnny Depp, Jerry Lewis, Faye Dunaway, Lili Taylor'
-  },
-  {
-    title: 'Forrest Gump',
-    cast: 'Tom Hanks, Robin Wright, Gary Sinise'
-  },
-  {
-    title: 'Rain Man',
-    cast: 'Dustin Hoffman, Tom Cruise, Valeria Golino, Gerald R. Molen'
-  },
-];
+app.use(bodyParser.json());
 
+// HTTP request logger printing time stamp and request type to terminal
 app.use(morgan('common'));
 
-app.get('/', (req, res) => {
-  res.send('welcome to movie api prototype');
-});
-
+// Routing requests for static files pointing on directory public
 app.use(express.static('public'));
 
+// Get a list of all movies
 app.get('/movies', (req, res) => {
-  res.json(myTopTenMovies);
+  res.status(200).send('Successful GET request returning data on all movies');
+});
+
+// Get data about a movie by title
+app.get('/movies/:movie', (req, res) => {
+  res.status(200).send('Successful GET request returning data on movie by title: ' + req.params.movie);
+});
+
+// Get data about a genre by name
+app.get('/movies/genres/:genre', (req, res) => {
+  res.status(200).send('Successful GET request returning on genre by name: ' + req.params.genre);
+});
+
+// Get data about a director by name
+app.get('/movies/directors/:director', (req, res) => {
+  res.status(200).send('Successful GET request returning on director by name: ' + req.params.director);
+});
+
+// Add new user
+app.post('/users', (req, res) => {
+  res.status(201).send('Successful POST request adding new user.');
+});
+
+// Update new user
+app.put('/users/:username', (req, res) => {
+  res.status(201).send('Successful PUT request updating new user: ' + req.params.username);
+});
+
+// Add a movie to users list of favorites
+app.post('/users/:username/movies/:movie', (req, res) => {
+  res.status(201).send('Successful POST request adding movie ' + req.params.movie + ' to favorites of user ' + req.params.username);
+});
+
+// Remove a movie to users list of favorites
+app.delete('/users/:username/movies/:movie', (req, res) => {
+  res.status(201).send('Successful DELETE request removing movie ' + req.params.movie + ' from favorites of user ' + req.params.username);
+});
+
+// Remove a user
+app.delete('/users/:username', (req, res) => {
+  res.status(201).send('Successful DELETE request removing user: ' + req.params.username);
 });
 
 app.use((err, req, res, next) => {
