4a3709f5f04b071e05df0232ad1f91c8479e0b69
olizi <o.zink@zink-projects.de>
2020-11-11 23:58:17 +0100

    adds new connection_uri

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const express = require('express');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 
